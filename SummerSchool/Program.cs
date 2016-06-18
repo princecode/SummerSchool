b9138e49923e10a700b3dec125a2c5e60d52ec02
Prince <princemwilliams@gmail.com>
2016-06-18 16:06:34 -0400

    Completed first take on basic menu.

--- SummerSchool/Program.cs
+++ SummerSchool/Program.cs
@@ -10,6 +10,25 @@ namespace SummerSchool
     {
         static void Main(string[] args)
         {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Please select an option from the menu below by entering the corresponding number.");
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("1. Enroll a student");
+            Console.WriteLine("2. Unenroll a student");
+            Console.WriteLine("3. Print out the list of enrolled students");
+            Console.WriteLine("4. Exit");          
+            Console.WriteLine();
+
+            Console.WriteLine(">");
+            Console.ReadLine();
+
+
+
+
+            Console.ReadLine(); 
         }
     }
 }
