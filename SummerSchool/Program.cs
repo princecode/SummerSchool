7cbf1ec54167ae25a1212194b5b3f6a84169dce3
Prince <princemwilliams@gmail.com>
2016-06-19 23:53:47 -0400

    Attempt to create method for enrollment... Also realizing tht my last two commits failed because I typed 'git -am' and forgot to include 'commit'.

--- SummerSchool/Program.cs
+++ SummerSchool/Program.cs
@@ -8,30 +8,65 @@ namespace SummerSchool
 {
     class Program
     {
-        static void Main(string[] args)
+        static string[] Roster = new string[15];
+
+        static void Main(string[] args)        
         {
+            while (true)
+            { 
             Console.WriteLine();
             Console.WriteLine();
-            Console.WriteLine("Please select an option from the menu below by entering the corresponding number.");
+            Console.WriteLine("Welcome to the School Enrollment System.");
+            Console.WriteLine("Term: Summer");
+            Console.WriteLine("Please enter a number to make a selection.");
 
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("1. Enroll a student");
             Console.WriteLine("2. Unenroll a student");
-            Console.WriteLine("3. Print out the list of enrolled students");
-            Console.WriteLine("4. Exit");          
+            Console.WriteLine("3. Print list of enrolled students");
+            Console.WriteLine("4. Exit");
             Console.WriteLine();
 
-            Console.WriteLine(">");
-            string menuEnt = Console.ReadLine();
-            //the below was just a test to make sure the above was capturing user input
-            //Need to write code to limit entires to the ones in the menu
-            //need to refactor add loops to catch invalid entires and maybe array for menu options
-            Console.WriteLine(menuEnt);
+            int menuEnt = Convert.ToInt32(Console.ReadLine());
+                if (menuEnt == 1)
+                {
+                    Enroll();
+                }
+                else if (menuEnt == 2)
+                {
+                   // Unenroll();
+                }
+                else if ( menuEnt == 3)
+                {
+                   // Print();
+                }
+                else if (menuEnt == 4)
+                {
+                   // Exit();
 
 
+                    Console.ReadLine();
 
-            Console.ReadLine(); 
+                }
+            }   
+         
         }
+        private static void Enroll()
+        {
+            string stu = Console.ReadLine();
+
+            for (int i = 0; i < 15; i++ )
+            {
+                if (Roster[i] == null)              
+                    {
+                        Roster[i] = stu;
+                        break;
+                    }
+                }
+            }
+
+        }
+
     }
-}
+
