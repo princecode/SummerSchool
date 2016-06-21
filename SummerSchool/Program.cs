c8e7013c818ea88ca63af4147d37299fb4b37679
Prince <princemwilliams@gmail.com>
2016-06-20 23:21:46 -0400

    Got way too carried away with visual ui tweaks using WriteLine. Cleaned up confirmation loop/cancel code.

--- SummerSchool/Program.cs
+++ SummerSchool/Program.cs
@@ -15,26 +15,44 @@ namespace SummerSchool
             while (true)
             {
                 //blank "Console.WriteLine();'s" are to create spacing/padding to help with visual presentation on ui
-                Console.WriteLine();
-                Console.WriteLine();
-                Console.WriteLine("___________________________________________________");
-                Console.WriteLine();
-                Console.WriteLine("Welcome to the School Enrollment System.");
-                Console.WriteLine("Term: Summer");
-                Console.WriteLine("Please enter a number to make a selection.");
+                // ui bank:
+                //Console.WriteLine("                                                   |");
+                //Console.WriteLine("***************************************************|");                
+                //Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX|");
+                //Console.WriteLine("---------------------------------------------------|");
+
+                //ui bank 2.0:
+                //Console.WriteLine("------------------------------------------------------------------|");
+
+
 
                 Console.WriteLine();
                 Console.WriteLine();
+                Console.WriteLine("---------------------------------------------------|");
+                Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX|");
+                Console.WriteLine("---------------------------------------------------|");
+                Console.WriteLine("                                                   |");
+                Console.WriteLine("Welcome to the School Enrollment System.           |");
+                Console.WriteLine("             Term: Summer                          |");
+                Console.WriteLine("Please enter a number to make a selection.         |");
+
+                Console.WriteLine("                                                   |");
+                Console.WriteLine("---------------------------------------------------|");
+                Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX|");
+                Console.WriteLine("---------------------------------------------------|");
+                Console.WriteLine("                                                   |");
+                Console.WriteLine("                                                   |");
                 //if (spot)
                 {
                 }
 
 
-                Console.WriteLine("1. Enroll a student");
-                Console.WriteLine("2. Unenroll a student");
-                Console.WriteLine("3. Print list of enrolled students");
-                Console.WriteLine("4. Exit");
-                Console.WriteLine();
+                Console.WriteLine("1. Enroll a student                                |");
+                Console.WriteLine("2. Unenroll a student                              |");
+                Console.WriteLine("3. Print list of enrolled students                 |");
+                Console.WriteLine("4. Exit                                            |");
+                Console.WriteLine("                                                   |");
+                Console.WriteLine("---------------------------------------------------|");
 
                 int menuEnt = Convert.ToInt32(Console.ReadLine());
                 if (menuEnt == 1)
@@ -56,98 +74,112 @@ namespace SummerSchool
                 }
                 else
                 {
+                    Console.WriteLine("---------------------------------------------------|");
+                    Console.WriteLine("                                                   |");
                     Console.ReadLine();
+                    Console.WriteLine("                                                   |");
+                    Console.WriteLine("---------------------------------------------------|");
+
                     // not sure what all is suposed to be here. or if I have/am missing {'s....
                 }
             }
         }
 
-            //---------------------------------------------------------------------------
+        //---------------------------------------------------------------------------
 
 
-            //static string Tuition()
-            //{
-            //    Console.WriteLine("200");
-            //}
+        //static string Tuition()
+        //{
+        //    Console.WriteLine("200");
+        //}
 
-           static void Enroll()
+        static void Enroll()
         {
-                Console.WriteLine("_________________________________________________");
-                Console.WriteLine();
-                Console.WriteLine();
-                Console.WriteLine("What's the name of the student to enroll?");
-                string stu = Console.ReadLine();
+            Console.WriteLine("---------------------------------------------------|");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("What's the name of the student to enroll?");
+            Console.WriteLine();
+            Console.WriteLine();
+            string stu = Console.ReadLine();
 
-                //put student in the next available spot in Students
-                int spot = GetNextAvialbleSpot();
+            //put student in the next available spot in Students
+            int spot = GetNextAvialbleSpot();
             // Check if enrollment is full, if so displays message and loops back to menu
-                if (spot == -1)
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("_________________________________________________");
-                    Console.WriteLine();
-                    Console.WriteLine();
-                    Console.WriteLine("School roster at capacity! ENROLLMENT IS CURRENTLY COLSED!");
-                    Console.WriteLine("Please press <ENTER> to return to the main menu.");
-                    Console.ReadKey();
-                }
-                // If not full display enroll confirmation
-                else
+            if (spot == -1)
             {
-                Console.WriteLine("You have entered: " + stu + ". Would you like to continue?");
-                Console.WriteLine("Please type 'Y' for 'Yes' to enroll this student or N' for 'no' to return to the menu?");
+                Console.WriteLine();
+                Console.WriteLine("---------------------------------------------------|");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("School roster at capacity! ENROLLMENT IS CURRENTLY COLSED!");
+                Console.WriteLine("Please press <ENTER> to return to the main menu.");
+                Console.ReadKey();
+            }
+            // If not full display enroll confirmation
+            else
+            {
+                Console.WriteLine("------------------------------------------------------------------|");
+                Console.WriteLine();
+                Console.WriteLine("                  -----------------                ");
+                Console.WriteLine("You have entered: |> " + stu + " <| Would you like to continue?");
+                Console.WriteLine("                  -----------------                ");
+                Console.WriteLine();
+                Console.WriteLine("Enter 'Y' to enroll this student or 'N' to return to the menu.");
+                Console.WriteLine();
                 // check for user confirm
                 string enrollconf = Console.ReadLine();
-                if (enrollconf.ToLower() == "y" )
+                if (enrollconf.ToLower() == "y")
                 {
                     // if confirmed display enrolee and tuition
                     Students[spot] = stu;
                     string tuition = "200";
-                    Console.WriteLine("_________________________________________________");
+                    Console.WriteLine("------------------------------------------------------------------|");
                     Console.WriteLine();
                     Console.WriteLine();
                     Console.WriteLine(stu + " has been enrolled at a tuition of £" + tuition);
+                    Console.WriteLine();
 
                 }
 
-            }              
-          
-                  
-            }
-            //method to find next available spot in students
-            static int GetNextAvialbleSpot()
-        {
-                for (int i = 0; i < Students.Length; i++)
-                {
-                    if (Students[i] == null)
-                    {
-                        //we found one!
-                        return i;
-                    }
-
-                }
-                // if we get through the whole 
-                return -1;
             }
 
-            //static void Unenroll()
-            //{
-            //    for (int = 0; i < Students.Length; i++)
-            //    {
-            //        if
-            //    }
-            //}
-            //private static void Print()
-            //{
-            //    for (int i = 0; i < Students.Length; i++)
-            //    {
-            //        Console.WriteLine(i + 1 + ". " + Students[i]);
-            //    }
-            //}
 
         }
+        //method to find next available spot in students
+        static int GetNextAvialbleSpot()
+        {
+            for (int i = 0; i < Students.Length; i++)
+            {
+                if (Students[i] == null)
+                {
+                    //we found one!
+                    return i;
+                }
+
+            }
+            // if we get through the whole 
+            return -1;
+        }
+
+        //static void Unenroll()
+        //{
+        //    for (int = 0; i < Students.Length; i++)
+        //    {
+        //        if
+        //    }
+        //}
+        //private static void Print()
+        //{
+        //    for (int i = 0; i < Students.Length; i++)
+        //    {
+        //        Console.WriteLine(i + 1 + ". " + Students[i]);
+        //    }
+        //}
 
     }
+
+}
 
 
 
